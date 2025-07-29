9fe1314935e5656d5e7d095e677c762fdaa9c1a6
root <root@SMEG2.edw.ee>
2025-07-29 19:47:09 -0400

    modified:   verify_mariabackup_SST.sh

--- verify_mariabackup_SST.sh
+++ verify_mariabackup_SST.sh
@@ -18,8 +18,8 @@ cp privileges_check.sql /tmp/
 chmod 644 /tmp/privileges_check.sql
 
 echo $(my_print_defaults --mysqld | grep wsrep_sst)
-MY_USER=$(my_print_defaults --mysqld| grep wsrep_sst_auth|sed 's/[^,=]*=//g'|cut -d: -f1)
-MY_PW=$(my_print_defaults --mysqld| grep wsrep_sst_auth|sed 's/[^,=]*=//g'|cut -d: -f2)
+MY_USER=$(my_print_defaults --mysqld| grep wsrep_sst_auth | tail -1 | sed 's/[^,=]*=//g'|cut -d: -f1)
+MY_PW=$(my_print_defaults --mysqld| grep wsrep_sst_auth | tail -1 | sed 's/[^,=]*=//g'|cut -d: -f2)
 if [ ! ${MY_USER} ]; then echo "The global wsrep_sst_auth is not in the configuration files."; exit 1; fi
 
 
@@ -31,7 +31,7 @@ fi
 
 if [ ! ${MY_PW} ]; then
   su - ${MY_USER} -c 'mariadb -Ae "select 1;"' 1>/dev/null 2>&1 || ERR=TRUE
-  if [ $ERR ]; then echo "User ${MY_USER} cannot login to the database."; exit 1; fi
+  if [ $ERR ]; then echo "User ${MY_USER} cannot login to the database or cannot switch user to ${MY_USER}."; exit 1; fi
   su - ${MY_USER} -c 'mariadb  -v -v -v < /tmp/privileges_check.sql' | grep -E "(^\+\-|^\|)"
 else
   mariadb -u "$MY_USER" -p"$MY_PW" -Ae "select 1;" 1>/dev/null 2>&1 || ERR=TRUE
